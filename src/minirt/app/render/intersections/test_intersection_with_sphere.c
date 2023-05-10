cde29bb0e690c2707181197d8985863faa8c5aa6
tdubois <tdubois@student.42angouleme.fr>
2023-05-11 01:42:38 +0200

    rewrote better sphere intersections fct
    see TODOs

--- src/minirt/app/render/intersections/test_intersection_with_sphere.c
+++ src/minirt/app/render/intersections/test_intersection_with_sphere.c
@@ -6,36 +6,54 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:25:15 by tdubois           #+#    #+#             */
-/*   Updated: 2023/05/10 17:28:18 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/05/11 01:40:52 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minirt/utils/geometry.h"
 #include <minirt/app/app.h>
 
+#include <minirt/utils/geometry.h>
 #include <stdbool.h>
 #include <math.h>
 
+//TODO: normalize thrown rays
+//TODO: create vec3_substract
+
+/**
+ * Test ray-sphere intersection. Algorithm is derived from
+ * https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+ *
+ * @param[in] ray Normalized ray
+ * @param[in] sphere
+ * @param[out] distance From ray origin to intersection point
+ * @returns Wether ray intersects with sphere
+ */
 bool	test_intersection_with_sphere(
 			t_ray const *ray,
 			t_sphere *const sphere,
 			float *distance)
 {
-	t_vec3	vec_to_center;
-	float	a;
-	float	b;
-	float	c;
-	float	quadratic_solution[2];
+	t_vec3	vec;
+	float	dot;
+	float	nabla;
 
-	vec_to_center = ray->pos;
-	vec3_sub(&vec_to_center, &sphere->pos);
-	a = vec3_dot(&ray->vec, &ray->vec);
-	b = 2.0f * vec3_dot(&vec_to_center, &ray->vec);
-	c = vec3_dot(&vec_to_center, &vec_to_center) - sphere->radius2;
-	if (solve_quadratic(a, b, c, quadratic_solution) == false)
+	vec3_substract(&vec, &ray->pos, &sphere->pos);
+	dot = vec3_dot(&ray->vec, &vec);
+	nabla = dot * dot - vec3_dot(&vec, &vec) + sphere->radius2;
+	if (nabla < 0)
 		return (false);
-	*distance = fmin(
-			fmax(0.0f, quadratic_solution[0]),
-			fmax(0.0f, quadratic_solution[1]));
-	return (*distance > 0.0f);
+	nabla = sqrtf(nabla);
+	*distance = -dot - nabla;
+	if (*distance > 0)
+	{
+		*distance /= vec3_dot(&ray->vec, &ray->vec);
+		return (true);
+	}
+	*distance = -dot + nabla;
+	if (*distance > 0)
+	{
+		*distance /= vec3_dot(&ray->vec, &ray->vec);
+		return (true);
+	}
+	return (false);
 }
