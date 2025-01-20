c9705de9a2bc63d583edb1b99b603c83481202a7
damienglld <damien17glld@outlook.com>
2025-01-20 12:04:24 +0100

    changed rgb_to_luma

--- src/minirt/app/utils/color/rgb_to_luma.c
+++ src/minirt/app/utils/color/rgb_to_luma.c
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   rgb_to_luma.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: damien <damien@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dguillau <dguillau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 20:16:51 by damien            #+#    #+#             */
-/*   Updated: 2025/01/19 00:16:07 by damien           ###   ########.fr       */
+/*   Updated: 2025/01/20 11:59:06 by dguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt/app/utils/color/color.h"
+#include "minirt/app/utils/geometry/geometry.h"
 
 float	rgb_to_luma(
 			t_vec3 rgb)
 {
-	t_vec3 norm;
-
-	vec3_scale_into(&norm, &rgb,  1.0 /255.0f);
-	return vec3_dot(&norm, &(t_vec3) {0.2126, 0.7152, 0.0722});
+	return vec3_dot_xyz(0.2126, 0.7152, 0.0722, &rgb);
 }
